67dcf3481fb6d90517cc92e133ee24a7eb54220d
Vanshika Rathod <85212298+vanshikaaaaaaaa@users.noreply.github.com>
2022-05-19 14:46:46 +0530

    Add files via upload

---
+++ dict_functions.py
@@ -0,0 +1,16 @@
+# Python program to demonstrate working
+# of dictionary copy
+dic = {1:'practise', 2:'python', 3:'program'}
+print('original: ', dic)
+ 
+# Accessing value for key
+print(dic.get(1))
+ 
+# Accessing keys for the dictionary
+print(dic.keys())
+ 
+# Accessing keys for the dictionary
+print(dic.values())
+ 
+# Printing all the items of the Dictionary
+print(dic.items())
---
+++ lists_functions.py
@@ -0,0 +1,13 @@
+list_1 = [50.29] 
+list_2 = [76.14, 89.64, 167.28] 
+print('list_1 length is ', len(list_1)) 
+print('list_2 length is ', len(list_2))
+fruits = ['apple', 'banana', 'cherry']
+fruits.insert(2, "pineapple")
+print(lists)
+x = fruits.count("cherry")
+print(x)
+fruits.reverse()
+print(fruits)
+x = fruits.copy()
+print(x)
